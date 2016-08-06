019840bf9b79bc5cc43376b46c26cfa7432ae931
Romil Lodaya <romillodaya03@gmail.com>
2016-08-06 17:51:45 +0530

    Add Display Message

---
+++ database/migrations/2016_08_06_120828_create_email_post.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailPost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function($table){
+            $table->string('email')->after('body');
+
+            $table->foreign('email')->references('email')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function($table){
+            $table->dropColumn('email');
+        });
+    }
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,11 @@
 @extends('app')
 @section('title', 'Login : Blog Using Laravel')
 @section('content')
-<header class="intro-header" style="background-image: url('{{ asset('components/img/post-bg.jpg') }}'); min-height:500px;">
+<header class="intro-header" style="background-image: url('{{ asset('components/img/post-bg.jpg') }}'); min-height:550px;">
     <div class="container">
         <div class="row">        
-            <div class="col-lg-4 col-lg-offset-4" style="margin-top:120px;">            
+            <div class="col-lg-4 col-lg-offset-4" style="margin-top:120px;">   
+                @include('message')         
                 <h3 class="text-center" style="color:#FFF;">Login to My Blog</h3>
                 <div class="auth">                
                     {!! Form::open() !!}
